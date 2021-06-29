477777d0f4c3035906b8f83676c612b4673c8dc4
xibertao <xibertao@borogroup.co.uk>
2021-06-29 08:37:41 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='boro_common',
+    version='0.1.0-alpha',
+    description='BORO Common',
+    author='BORO Solutions',
+    author_email='xibertao@borogroup.co.uk',
+    url='https://github.com/boro-alpha/boro_common.git',
+    packages=setuptools.find_packages(),
+    include_package_data=True)
